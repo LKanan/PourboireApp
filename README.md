b138069bc53c7372240c29c65f8fd101f0ed6f12
LKanan <lucienkanani@gmail.com>
2025-01-23 12:23:06 +0200

    phase3 done

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PourboireApp
 Application jetpack compose pour gerer les pourboires
+![img.png](Photo du resultat final)
--- app/src/main/java/com/example/pourboire/MainActivity.kt
+++ app/src/main/java/com/example/pourboire/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
@@ -16,6 +17,7 @@ import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
@@ -24,6 +26,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -32,6 +35,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.pourboire.ui.theme.PourboireTheme
 import java.text.NumberFormat
+
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.KeyboardType
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,8 +62,16 @@ fun onCreate(){
     }
 }
 
+private fun calculateTip(amount: Double, tipPercent:Double=15.0):String{
+    val tip=tipPercent/100*amount
+    return NumberFormat.getCurrencyInstance().format(tip)
+}
+
 @Composable
 fun TipTimeLayout(){
+    var amountInput by remember { mutableStateOf("") }
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount)
     Column (
         modifier = Modifier
             .statusBarsPadding()
@@ -71,9 +87,15 @@ fun TipTimeLayout(){
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
-        EditNumberFiefd(modifier = Modifier)
+        EditNumberField(
+            value = amountInput,
+            onValueChange = { amountInput = it },
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
+        )
         Text(
-            text=stringResource(R.string.tip_amount,"$0.00"),
+            text=stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
         )
         Spacer(modifier = Modifier.height(150.dp))
@@ -81,16 +103,18 @@ fun TipTimeLayout(){
 }
 
 @Composable
-fun EditNumberFiefd(modifier: Modifier){
+fun EditNumberField(value:String, onValueChange:(String)-> Unit, modifier: Modifier= Modifier){
+    var amountInput by remember { mutableStateOf("") }
     TextField(
-        value = "",
-        onValueChange = {},
-        modifier=modifier
+        value = value,
+        onValueChange = onValueChange,
+        modifier=modifier,
+        label = {
+            Text(stringResource(R.string.bill_amount))
+            },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
     )
 }
 
-private fun calculateTip(amount: Double, tipPercent:Double=15.0):String{
-    val tip=tipPercent/100*amount
-    return NumberFormat.getCurrencyInstance().format(tip)
-}
 
